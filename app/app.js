7d050aa7621ae80477d1d0e431d63127921a461b
wtitze <w.titze@gmail.com>
2018-11-09 09:52:12 +0000

    controllo sull'esistenza della capitale

--- app/app.js
+++ app/app.js
@@ -24,16 +24,23 @@ app.get('/sendContinent', function (req, res) {
 
 app.get('/sendCapital', function (req, res) {
   var city = req.query.capital;
+  var found = false;
   countries.rawData.forEach(function(nazione) {
-    if (city === nazione.properties.capital)
-      res.render('result', { message: "la nazione è: ", result: nazione.properties.country}); 
-  });});   
+    if (city === nazione.properties.capital) {
+      found = true;
+      res.render('result', { message: "la nazione che ha come capitale " + city + " è: ", result: nazione.properties.country});   
+    }
+  });
+  if (!found) {
+      res.render('result', { message: "la nazione che ha come capitale " + city + " è: ", result: "inesistente"}); 
+  }
+});   
 
 app.get('/sendCountry', function (req, res) {
   var country = req.query.country;
   countries.rawData.forEach(function(nazione) {
     if (country === nazione.properties.country)
-      res.render('result', { message: "la capitale è: ", result: nazione.properties.capital}); 
+      res.render('result', { message: "la capitale della nazione " + country + " è: ", result: nazione.properties.capital}); 
   });
 });  
 
